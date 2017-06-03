e1105008895ea29c95b1532fc3c2fce620b7f582
allamchz <allam@una.cr>
2017-06-03 10:28:18 -0600

    Subida

--- app/src/main/java/una/ac/cr/gitexample/MainActivity.java
+++ app/src/main/java/una/ac/cr/gitexample/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_andorid_xxx);
+        setContentView(R.layout.activity_andorid_estudio_again);
     }
 }
